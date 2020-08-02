2c67d7487e789293c6dfd14c943425411b5eb9c6
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-08-02 13:25:10 +0800

    fix HelloController

--- src/main/java/cn/lmsite/lmfavor/web/api/home/HelloController.java
+++ src/main/java/cn/lmsite/lmfavor/web/api/home/HelloController.java
@@ -2,9 +2,7 @@ package cn.lmsite.lmfavor.web.api.home;
 
 import cn.lmsite.lmfavor.commons.result.BaseResult;
 import cn.lmsite.lmfavor.enums.CommonResultEnum;
-import cn.lmsite.lmfavor.vo.user.UserRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,11 +19,10 @@ public class HelloController {
     /**
      * 用户注册
      *
-     * @param userRequest 用户请求
      * @return {@link BaseResult<Integer>}
      */
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public BaseResult<String> userRegister(@RequestBody UserRequest userRequest) {
+    public BaseResult<String> userRegister() {
         return new BaseResult<>(CommonResultEnum.A0000, "你好， LmFavor！接口访问成功！\n敬请使用 - @DilysLyu ");
     }
 }
